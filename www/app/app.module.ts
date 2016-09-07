3d44f1052f122000e61311c2b808deae6b79023b
davembush <davembush@dmbcllc.com>
2016-09-07 06:37:05 -0400

    Removed Components module from app module

--- www/app/app.module.ts
+++ www/app/app.module.ts
@@ -5,11 +5,10 @@ import { HttpModule }     from '@angular/http';
 import { AppComponent }  from './app.component';
 import { routing }        from './app.routes';
 import { View } from './view/view';
-import {ComponentsModule} from './components/components.module';
 import {EditModule} from "./edit/edit.module";
 
 @NgModule({
-    imports:      [ BrowserModule, HttpModule, routing , ReactiveFormsModule, ComponentsModule, EditModule],
+    imports:      [ BrowserModule, HttpModule, routing , ReactiveFormsModule, EditModule],
     declarations: [ View, AppComponent ],
     bootstrap:    [ AppComponent ]
 })
